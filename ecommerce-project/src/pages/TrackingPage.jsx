e0d198ca4d7dc4be672a9c674d0b71fa14ddc3de
c-chin <c-chin@usen-next.jp>
2026-08-04 00:53:42 +0800

    change a to link

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,5 +1,6 @@
 import { Header } from '../components/Header'
 import './TrackingPage.css'
+import { Link } from 'react-router'
 
 export function TrackingPage() {
 
@@ -7,14 +8,14 @@ export function TrackingPage() {
         <>
             <title>Tracking</title>
             <link rel="icon" type="image/svg+xml" href="tracking-favicon.png" />
-            
+
             <Header />
 
             <div className="tracking-page">
                 <div className="order-tracking">
-                    <a className="back-to-orders-link link-primary" href="/orders">
+                    <Link className="back-to-orders-link link-primary" to="/orders">
                         View all orders
-                    </a>
+                    </Link>
 
                     <div className="delivery-date">
                         Arriving on Monday, June 13
